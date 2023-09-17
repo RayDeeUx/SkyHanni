88ccbd5532d3e6fc01ed43541bdfd5ddb3a1be7e
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-09-17 02:09:13 -0400

    Update WikiCommand.kt to remove comments

--- src/main/java/at/hannibal2/skyhanni/features/commands/WikiCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/WikiCommand.kt
@@ -25,10 +25,6 @@ class WikiCommand {
                 event.isCanceled = true
                 val search = packet.message.substring(6)
                 LorenzUtils.chat("§e[SkyHanni] Searching the Fandom Wiki for §c$search")
-
-                //val url = "https://www.google.com/search?q=inurl%3Ahypixel-skyblock.fandom.com $search&hl=en"
-                //val poggers = true
-                //val google = !poggers
                 val url = "https://hypixel-skyblock.fandom.com/wiki/Special:Search?query=$search&scope=internal"
                 OSUtils.openBrowser(url.replace(' ', '+'))
             }
