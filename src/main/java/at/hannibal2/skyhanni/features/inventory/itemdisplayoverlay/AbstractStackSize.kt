6d265a3ee4fb5dd034142fcd62e800d0d2d6e36c
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-05 18:35:28 -0500

    [cherrypick everywhere] new func to be used

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
@@ -14,11 +14,15 @@ abstract class AbstractStackSize {
         event.stackTip = getStackTip(event.stack)
     }
     abstract fun getStackTip(item: ItemStack): String
+    fun Pattern.returnGroupFromString(groupName: String, input: String): String {
+        this.matchMatcher(input) { return group(groupName) }
+        return ""
+    }
 }
 
 abstract class AbstractMenuStackSize : AbstractStackSize()  {
     val configMenuStackSize get() = configItemStackSize.stackSize.menu
-    
+
     fun Pattern.returnPercentFromLoreLineAsStackSize(line: String): String {
         this.matchMatcher(line) {
             return group("percent").replace("100", "§a✔")
