1cb11fac10e424f7bfebd99fe31aa4e404376bc5
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-05 20:21:24 -0500

    Revert "[cherrypick IDOF]"
    This reverts commit 3f1e43e78ead804e45705cebe658f8234e9207b6.

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/ItemDisplayOverlayFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/ItemDisplayOverlayFeatures.kt
@@ -435,7 +435,10 @@ object ItemDisplayOverlayFeatures : AbstractStackSize() {
 
     private fun isShredder(internalName: NEUInternalName): Boolean = SHREDDER.isSelected() && internalName == shredderInternalName
     private fun getShredderTip(lore: List<String>): String {
-        return shredderBonusDamageLoreLinePattern.returnGroupFromItemLore("dmgbonus", lore)
+        for (line in lore) {
+            return shredderBonusDamageLoreLinePattern.returnGroupFromString("dmgbonus", line)
+        }
+        return ""
     }
 
     private fun isMinionStorage(item: ItemStack): Boolean = STORAGE_TIER.isSelected() && storageChestInternalNamePattern.matches(item.getInternalName()) && storageChestItemNamePattern.matches(item.cleanName())
