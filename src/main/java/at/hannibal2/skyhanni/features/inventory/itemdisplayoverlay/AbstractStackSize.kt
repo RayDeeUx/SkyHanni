42e7ccc2d949b799b502aeb87dfed4167aadf9b6
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-05 20:21:44 -0500

    Revert "[cherrypick everywhere] new func to be used"
    This reverts commit 6d265a3ee4fb5dd034142fcd62e800d0d2d6e36c.

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
@@ -14,15 +14,11 @@ abstract class AbstractStackSize {
         event.stackTip = getStackTip(event.stack)
     }
     abstract fun getStackTip(item: ItemStack): String
-    fun Pattern.returnGroupFromString(groupName: String, input: String): String {
-        this.matchMatcher(input) { return group(groupName) }
-        return ""
-    }
 }
 
 abstract class AbstractMenuStackSize : AbstractStackSize()  {
     val configMenuStackSize get() = configItemStackSize.stackSize.menu
-
+    
     fun Pattern.returnPercentFromLoreLineAsStackSize(line: String): String {
         this.matchMatcher(line) {
             return group("percent").replace("100", "§a✔")
